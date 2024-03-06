6be4c0c35342f4234480b5cd15302991cb8a6432
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-03-06 18:24:37 +0530

    huh

--- 9618_w23_qp_41/9618_w23_sf_41/11_9618_41_SourceFiles/Question2_N23-my-answer.py
+++ 9618_w23_qp_41/9618_w23_sf_41/11_9618_41_SourceFiles/Question2_N23-my-answer.py
@@ -1,7 +1,8 @@
 # a) i)
-Queue = [] # 50 ineteger
-HeadPointer = -1 # integer
-TailPointer = 0 # integer
+Queue = []  # 50 ineteger
+HeadPointer = -1  # integer
+TailPointer = 0  # integer
+
 
 # a) ii)
 def Enqueue(value):
@@ -14,6 +15,7 @@ def Enqueue(value):
         if HeadPointer == -1:
             HeadPointer = 0
 
+
 # a) iii)
 def Dequeue():
     global Queue, HeadPointer, TailPointer
@@ -22,7 +24,8 @@ def Dequeue():
         return "Empty"
     else:
         HeadPointer += 1
-        return Queue[HeadPointer-1]
+        return Queue[HeadPointer - 1]
+
 
 # b)
 def ReadData():
@@ -35,57 +38,56 @@ def ReadData():
     except Exception as e:
         print(f"Another error occured: {e}")
 
+
 # c) i)
 class RecordData:
     # self.ID string
     # self.Total integer
-    def __init__(self, ID, Total):
+    def __init__(self, ID=None, Total=None):
         self.ID = ID
         self.Total = Total
+
+
 # c) ii
-Records = [] # 50 elements of type RecordData
-NumberRecords = 0 # integer
+Records = []  # 50 elements of type RecordData
+# Records = [RecordData(ID=None, Total=None)] * 50  # 50 elements of type RecordData
+NumberRecords = 0  # integer
+
 
 # c) iii)
 def TotalData():
-    DataAccessed = Dequeue()
+    global NumberRecords, Records
+
     Flag = False
+    DataAccessed = Dequeue()
+
     if NumberRecords == 0:
         Records.append(RecordData(ID=DataAccessed, Total=1))
-        Falg = True
+        Flag = True
         NumberRecords += 1
     else:
         for x in range(0, NumberRecords):
             if Records[x].ID == DataAccessed:
                 Records[x].Total += 1
                 Flag = True
-    if Flag is False:
-        Records[NumberRecords].ID = DataAccessed
-        Records[NumberRecords].Total = 1
-        NumberRecords += 1
-        
-                
-
-    
+        if Flag is False:
+            Records.append(RecordData(ID=DataAccessed, Total=1))
+            NumberRecords += 1
 
 
+def OutputRecords():
+    already_printed = []
+    for x in range(0, NumberRecords):
+        record = Records[x]
+        if not ((record.ID == "") or (record.Total == 0)):
+            if not (record.ID in already_printed):
+                print(f"ID {record.ID} Total {record.Total}")
+                already_printed.append(record.ID)
+    print(already_printed)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ReadData()
+# PLEASE REMEMBER THIS PART! PLEASE!!!
+while HeadPointer != TailPointer:
+    TotalData()
+    OutputRecords()
